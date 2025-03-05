f4d93da6d5d54e30ae68bed8da40bdd4dae38644
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-03-05 16:09:14 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,9 +4,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="">
         {children}
       </body>
     </html>
